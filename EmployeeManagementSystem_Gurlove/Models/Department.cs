f3967e9319d861d194d5b0e1822c764d8e5e690d
singhnirmal128@gmail.com <singhnirmal128@gmail.com>
2020-12-03 07:00:30 +0530

    Add project files.

---
+++ EmployeeManagementSystem_Gurlove/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem_Gurlove_09.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<EmployeeDetail> EmployeeDetail { get; set; }
+    }
+}
---
+++ EmployeeManagementSystem_Gurlove/Models/Designation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem_Gurlove_09.Models
+{
+    public class Designation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public List<EmployeeDetail> EmployeeDetail { get; set; }
+
+    }
+}
